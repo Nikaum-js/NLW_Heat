422bef12bcb74e707baee4fa7b4a54b343ea2eea
Nikolas.Dev <nikkolas21082003@gmail.com>
2021-10-30 17:38:48 -0300

    Update GetLast3MessagesController.ts

--- src/controllers/GetLast3MessagesController.ts
+++ src/controllers/GetLast3MessagesController.ts
@@ -4,7 +4,6 @@ import { GetLast3MessagesService } from '../services/GetLast3MessagesService'
 class GetLast3MessagesController {
   async handle(request: Request, response: Response) {
     const service = new GetLast3MessagesService();
-
     const result = await service.execute();
 
     return response.json(result);
